28fefd747b7c2ee1898ecc67087b8dbdc53c5193
João Henrique <joao.costa.herique@uel.br>
2025-09-28 20:40:29 -0300

    pilha.h, pilha.c

---
+++ src/lib/pilha/pilha.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pilha.h"
+
+typedef struct Nod {
+    void* valor;
+    struct Nod* prox;
+} Nod;
+
+typedef Nod* Node;
+
+typedef struct {
+    Node topo;
+    int tamanho;
+} Pilha;
+
+PILHA criarPilha(){
+    Pilha* pilha = (Pilha*)malloc(sizeof(Pilha));
+    if(!pilha)return NULL;
+
+    pilha->topo = NULL;
+    pilha->tamanho = 0;
+
+    return pilha;
+}
+
+void push(PILHA pilha_g, void* valor){
+    Pilha* pilha = (Pilha*)pilha_g;
+    if(!pilha)return;
+
+    Node novo = (Node)malloc(sizeof(struct Nod));
+    if(!novo)return;
+
+    novo->valor = valor;
+    novo->prox = pilha->topo->prox;
+    pilha->topo = novo;
+    pilha->tamanho++;
+}
+
+int pilha_vazia(const PILHA pilha_g){
+    Pilha* pilha = (Pilha*) pilha_g;
+    if(!pilha)return 1;
+
+    return pilha->tamanho == 0;
+}
+
+PILHA pop(PILHA pilha_g){
+    Pilha* pilha = (Pilha*) pilha_g;
+    if(!pilha || pilha_vazia(pilha))return NULL;
+
+    Node remove = pilha->topo;
+    pilha->topo = remove->prox;
+    free(remove);
+    pilha->tamanho--;
+}
+
+int pilha_tamanho(const PILHA pilha_g){
+    Pilha* pilha = (Pilha*) pilha_g;
+    if(!pilha)return 0;
+
+    return pilha->tamanho;
+}
+
+void destruirPilha(PILHA pilha_g){
+    Pilha* pilha = (Pilha*)pilha_g;
+    if(!pilha)return;
+
+    while(pilha->tamanho > 0){
+        pop(pilha);
+    }
+}
